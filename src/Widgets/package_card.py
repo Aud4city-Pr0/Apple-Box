18d59a7f1333fbea32ef6d5c5c1040ae65133560
Aud4city-Pr0 <zachary.duriancik@gmail.com>
2026-07-22 17:26:35 -0400

    added package_card code

---
+++ src/Widgets/package_card.py
@@ -0,0 +1,46 @@
+# other python modules
+import sys
+
+# importing pyside files
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile, QIODevice
+from PySide6.QtWidgets import QWidget, QLabel, QComboBox
+
+# the package card class
+class PackageCard(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.package_name = ""
+        self.versions = []
+
+        # loading ui
+        ui_file = QFile("src/Ui/PackageCard.ui")
+        if not ui_file.open(QFile.ReadOnly):
+            # error has occured
+            print(f"Oh noes! ;( looks like the file: {ui_file.fileName()}, could not be loaded because of error: {ui_file.errorString()}")
+            sys.exit(-1)
+        
+        # using PySide6's ui loader
+        loader = QUiLoader()
+        self.card_ui = loader.load(ui_file)
+        ui_file.close()
+
+        # getting important components like, name, description, and version widgets
+        self.name_lable = self.card_ui.findChild(QLabel, "Name")
+        self.description_lable = self.card_ui.findChild(QLabel, "Description")
+        self.versions_box = self.card_ui.findChild(QComboBox, "VersionsBox")
+
+    def set_name(self, name):
+        self.package_name = name
+        self.name_lable.setText(self.package_name)
+
+    def set_name(self, desc):
+        self.description_lable.setText(desc)
+
+    def set_versions(self, vers):
+        self.versions = vers
+        for ver in self.versions:
+            print(f"Version: {ver}")
+            self.versions_box.addItem("Version: " + ver)
+
+    
